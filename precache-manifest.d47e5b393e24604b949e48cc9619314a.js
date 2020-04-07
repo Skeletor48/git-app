0b2de91ac845fa7e4b68b564dc445b9d921a69ce
Mihaly Banfi <mihaly.banfi@tappointment.com>
2020-04-07 17:29:33 +0200

    Updates

---
+++ precache-manifest.d47e5b393e24604b949e48cc9619314a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31320bcb6b3c2abac8340731e8ee7bbf",
+    "url": "/git-app/index.html"
+  },
+  {
+    "revision": "b561505e80722cb14931",
+    "url": "/git-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3af42f436a8665ded66c",
+    "url": "/git-app/static/js/2.464bfd68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-app/static/js/2.464bfd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b561505e80722cb14931",
+    "url": "/git-app/static/js/main.3064a4be.chunk.js"
+  },
+  {
+    "revision": "30a700207f86f8cfc84c",
+    "url": "/git-app/static/js/runtime-main.938b7aeb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
